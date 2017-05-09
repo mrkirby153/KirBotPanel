864942849517f963a9c017d56b643e3f91f6d7ce
mrkirby153 <mr.austinwhyte@gmail.com>
2017-05-09 16:39:43 -0700

    Remove default values from channels columns

--- database/migrations/2017_05_06_173758_create_music_settings.php
+++ database/migrations/2017_05_06_173758_create_music_settings.php
@@ -16,8 +16,8 @@ class CreateMusicSettings extends Migration
             $table->string('id')->primary();
             $table->boolean('enabled')->default(true);
             $table->enum('mode', ['OFF','WHITELIST', 'BLACKLIST'])->default('OFF');
-            $table->text('channels')->default('');
-            $table->text('blacklist_songs')->default('')->nullable();
+            $table->text('channels')->nullable();
+            $table->text('blacklist_songs')->nullable();
             $table->integer('max_queue_length')->default(-1);
             $table->integer('max_song_length')->default(-1);
             $table->integer('skip_cooldown')->default(0);
