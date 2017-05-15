47719c4b8152d44a72d17376327b640b5d7bc4dc
mrkirby153 <mr.austinwhyte@gmail.com>
2017-05-14 19:23:06 -0700

    Fix error when logging in

--- app/UserRepository.php
+++ app/UserRepository.php
@@ -15,7 +15,7 @@ class UserRepository {
         return User::updateOrCreate(['id' => $request->id], [
             'id' => $request->id,
             'username' => $user['username'],
-            'email' => $user['email'],
+            'email' => $request->email,
             'refresh_token' => $request->refreshToken,
             'token_type' => in_array('guilds', explode(' ', $request->accessTokenResponseBody['scope']))? 'NAME_SERVERS' : 'NAME_ONLY',
             'token' => $request->token,
