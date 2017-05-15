ff5aff25b1573301be2b78b9b25ac2aa5f067bc0
mrkirby153 <mr.austinwhyte@gmail.com>
2017-05-14 20:05:35 -0700

    Fix error if the user hasn't verified their email
    The Discord API returns null for an unverified email. The panel requries an email to be set, therefore we set the email to a randomly generated one based on a pattern if the user doesn't have an email set.

--- app/UserRepository.php
+++ app/UserRepository.php
@@ -5,6 +5,7 @@ namespace App;
 
 
 use Carbon\Carbon;
+use Keygen;
 
 class UserRepository {
 
@@ -15,9 +16,9 @@ class UserRepository {
         return User::updateOrCreate(['id' => $request->id], [
             'id' => $request->id,
             'username' => $user['username'],
-            'email' => $request->email,
+            'email' => ($request->email == null) ? 'unverified-' . substr($user['username'], 0, 3) . '-' . Keygen::alphanum(10)->generate() . '@kirbot' : $request->email,
             'refresh_token' => $request->refreshToken,
-            'token_type' => in_array('guilds', explode(' ', $request->accessTokenResponseBody['scope']))? 'NAME_SERVERS' : 'NAME_ONLY',
+            'token_type' => in_array('guilds', explode(' ', $request->accessTokenResponseBody['scope'])) ? 'NAME_SERVERS' : 'NAME_ONLY',
             'token' => $request->token,
             'expires_in' => $expiresIn->toDateTimeString()
         ]);
