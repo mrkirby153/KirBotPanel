7cc0fd640511c0cbcfe256c5c980707439203cfb
mrkirby153 <mr.austinwhyte@gmail.com>
2017-08-06 22:13:59 -0700

    Add channel logging scaffolding

---
+++ app/ServerMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServerMessage extends Model {
+
+    protected $table = "server_messages";
+
+    public $incrementing = false;
+}
