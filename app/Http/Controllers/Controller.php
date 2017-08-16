805eb2a0429f57ce36e9065119b1eb09d994146d
mrkirby153 <mr.austinwhyte@gmail.com>
2017-08-16 11:57:55 -0700

    Remove dependence on robot's REST API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Channel;
 use App\ServerSettings;
-use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -61,44 +60,49 @@ class Controller extends BaseController {
     }
 
     protected function getChannelsFromBot($server) {
-        $client = new \GuzzleHttp\Client();
-        try {
-            $response = $client->request('GET', env('KIRBOT_URL') . 'v1/channels/' . $server, [
-                'connect_timeout' => $this->connect_timeout
-            ]);
-            $channels = json_decode($response->getBody());
-            return $channels != null? $channels : Channel::whereServer($server)->get();
-        } catch (ConnectException $exception) {
-            return Channel::whereServer($server)->get();
-        }
+        /*        $client = new \GuzzleHttp\Client();
+                try {
+                    $response = $client->request('GET', env('KIRBOT_URL') . 'v1/channels/' . $server, [
+                        'connect_timeout' => $this->connect_timeout
+                    ]);
+                    $channels = json_decode($response->getBody());
+                    return $channels != null? $channels : Channel::whereServer($server)->get();
+                } catch (ConnectException $exception) {
+                    return Channel::whereServer($server)->get();
+                }*/
+        return Channel::whereServer($server)->get();
 
     }
 
     protected function getTextChannelsFromBot($server) {
-        $client = new \GuzzleHttp\Client();
-        try{
-            $response = $client->request('GET', env('KIRBOT_URL') . 'v1/channels/' . $server . '/text', [
-                'connect_timeout' => $this->connect_timeout
-            ]);
-            $channels = json_decode($response->getBody());
-            return $channels != null? $channels : Channel::whereServer($server)->whereType('TEXT')->get();
-        } catch(ConnectException $exception){
-            return Channel::whereServer($server)->whereType('TEXT')->get();
-        }
+        /*        $client = new \GuzzleHttp\Client();
+                try {
+                    $response = $client->request('GET', env('KIRBOT_URL') . 'v1/channels/' . $server . '/text', [
+                        'connect_timeout' => $this->connect_timeout
+                    ]);
+                    $channels = json_decode($response->getBody());
+                    return $channels != null ? $channels : Channel::whereServer($server)->whereType('TEXT')->get();
+                } catch (ConnectException $exception) {
+                    return Channel::whereServer($server)->whereType('TEXT')->get();
+                }*/
+
+        return Channel::whereServer($server)->whereType('TEXT')->get();
 
     }
 
     protected function getVoiceChannelsFromBot($server) {
-        $client = new \GuzzleHttp\Client();
-        try{
-            $response = $client->request('GET', env('KIRBOT_URL') . 'v1/channels/' . $server . '/voice', [
-                'connect_timeout' => $this->connect_timeout
-            ]);
-            $channels = json_decode($response->getBody());
-            return $channels != null? $channels : Channel::whereServer($server)->get();
-        } catch(ConnectException $exception){
-            return Channel::whereServer($server)->whereType('VOICE')->get();
-        }
+        /*        $client = new \GuzzleHttp\Client();
+                try {
+                    $response = $client->request('GET', env('KIRBOT_URL') . 'v1/channels/' . $server . '/voice', [
+                        'connect_timeout' => $this->connect_timeout
+                    ]);
+                    $channels = json_decode($response->getBody());
+                    return $channels != null ? $channels : Channel::whereServer($server)->get();
+                } catch (ConnectException $exception) {
+                    return Channel::whereServer($server)->whereType('VOICE')->get();
+                }*/
+
+        return Channel::whereServer($server)->whereType('VOICE')->get();
 
     }
 }
