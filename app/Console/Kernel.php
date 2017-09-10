83df5f51b67edb6e88b70b2f795ed65f3d5f1201
mrkirby153 <mr.austinwhyte@gmail.com>
2017-09-10 13:15:38 -0700

    Commit messages from redis to the database

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\ProcessMessages;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call(function(){
+            ProcessMessages::dispatch();
+        })->hourly();
     }
 
     /**
