17ccbc4cebfde0e1fe926e9dbbfa4dff73c9cdd4
mrkirby153 <mr.austinwhyte@gmail.com>
2018-02-22 15:39:20 -0800

    Update to Laravel 5.6

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,5 @@
 window._ = require('lodash');
+window.Popper = require('popper.js').default;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -8,8 +9,6 @@ window._ = require('lodash');
 
 try{
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 }catch(e) {}
 
 /**
@@ -47,5 +46,7 @@ window.Panel = {};
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     encrypted: true
 // });
