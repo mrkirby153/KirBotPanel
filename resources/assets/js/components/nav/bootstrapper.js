e1e78c2f47e95c2adaa784963a23170ab11af1c7
mrkirby153 <mr.austinwhyte@gmail.com>
2018-06-29 16:16:24 -0700

    Add components to wrap navbar components

---
+++ resources/assets/js/components/nav/bootstrapper.js
@@ -0,0 +1,6 @@
+// Include neccecary components
+let navbarItem = require('./NavbarLink');
+let navbarDropdown = require('./NavbarDropdown');
+
+Vue.component('navbar-link', navbarItem);
+Vue.component('navbar-dropdown', navbarDropdown);
