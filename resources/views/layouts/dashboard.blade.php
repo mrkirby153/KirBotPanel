cea766ed60eb1aeb443e8ed78f2418f032f54d7a
mrkirby153 <mr.austinwhyte@gmail.com>
2018-07-02 19:35:09 -0700

    Use cards for better looking layouts

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,18 +3,16 @@
 @section('title', $server->name)
 
 @section('content')
-    <div class="row mt-5">
-        <div class="col-md-6 offset-md-3 offset-sm-1 col-sm-10">
-            <div class="row">
-                <div class="col-lg-3 col-md-12">
-                    <div class="server-sidebar mb-3 pb-2">
-                        <div class="d-flex flex-column justify-content-center">
-                            <img class="m-auto server-image" src="{{$server->getIcon()}}"/>
-                            <span class="text-center mt-2">{{$server->name}}</span>
-                        </div>
-                    </div>
-                    <ul class="nav flex-column nav-pills nav-fill dashboard-sidebar">
-                        {{-- TODO: Replace this with the actual links --}}
+    <div class="row mt-2">
+        {{-- Sidebar --}}
+        <div class="col-lg-2 col-md-12">
+            <div class="mb-3 pb-2 card">
+                <div class="card-header">
+                    {{$server->name}}
+                </div>
+                <div class="card-body d-flex flex-column">
+                    <img class="m-auto server-image" src="{{$server->getIcon()}}">
+                    <ul class="nav nav-pills nav-fill flex-column mt-3 dashboard-sidebar">
                         <li class="nav-item">
                             <a class="nav-link active text-left" href="#">General</a>
                         </li>
@@ -29,10 +27,21 @@
                         </li>
                     </ul>
                 </div>
-                <div class="col-md-9">
+            </div>
+        </div>
+        {{-- End Sidebar --}}
+
+        {{-- Main Content --}}
+        <div class="col-lg-10 col-md-12 pb-sm-2">
+            <div class="card">
+                <div class="card-header">
+                    General
+                </div>
+                <div class="card-body">
                     @yield('panel')
                 </div>
             </div>
         </div>
+        {{-- End Main Content --}}
     </div>
 @endsection
