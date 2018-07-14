bbf8428b71dcbeaaf2699957a6eb9cd085e3cd92
mrkirby153 <mr.austinwhyte@gmail.com>
2018-07-13 18:19:38 -0700

    Implement censor settings

--- resources/assets/js/components/dashboard/Spam.js
+++ resources/assets/js/components/dashboard/Spam.js
@@ -7,21 +7,25 @@ Vue.component('settings-spam', {
             editor: null,
             loading: false,
             success: false,
-            fail: false,
-            readonly: ReadOnly,
+            readonly: App.readonly,
             changed: false,
-            original: ""
+            original: "",
+            confirmDiscard: false
         }
     },
 
     mounted() {
-        let container = document.getElementById("jsoneditor");
+        let container = document.getElementById("spam-settings");
         let vm = this;
         this.editor = new JSONEditor(container, {
             mode: 'code',
             onEditable(node) {
-                if (!node.path)
-                    return !ReadOnly
+                if (!node.path) {
+                    if (this.busy) {
+                        return false;
+                    }
+                    return !this.readonly
+                }
             },
             onChange() {
                 vm.changed = true
@@ -34,19 +38,18 @@ Vue.component('settings-spam', {
 
     methods: {
         discard() {
-            let vm = this;
-            $("#confirm-revert").modal({
-                onApprove() {
-                    vm.editor.set(vm.original);
-                    vm.changed = false;
-                }
-            }).modal('show');
+            if (this.confirmDiscard) {
+                this.changed = false;
+                this.editor.set(this.original);
+                this.confirmDiscard = false;
+            } else {
+                this.confirmDiscard = true;
+            }
 
         },
         save() {
             this.loading = true;
             this.success = false;
-            this.fail = false;
             let json = JSON.stringify(this.editor.get());
             console.log("Saving json " + json);
             axios.patch('/dashboard/' + Server.id + '/spam', {
@@ -60,7 +63,7 @@ Vue.component('settings-spam', {
                     vm.success = false;
                 }, 10000);
             }).catch(resp => {
-                this.fail = true;
+                toastr["error"]("An unknown error occurred. Please try again");
                 this.loading = false;
             })
         }
@@ -73,21 +76,25 @@ Vue.component('settings-censor', {
             editor: null,
             loading: false,
             success: false,
-            error: false,
-            readonly: ReadOnly,
+            readonly: App.readonly,
             changed: false,
-            original: ""
+            original: "",
+            confirmDiscard: false
         }
     },
 
     mounted() {
-        let container = document.getElementById("censor-editor");
+        let container = document.getElementById("censor-settings");
         let vm = this;
         this.editor = new JSONEditor(container, {
             mode: 'code',
             onEditable(node) {
-                if (!node.path)
-                    return !ReadOnly
+                if (!node.path){
+                    if(vm.loading){
+                       return false;
+                    }
+                    return !vm.readonly
+                }
             },
             onChange() {
                 vm.changed = true
@@ -100,19 +107,18 @@ Vue.component('settings-censor', {
 
     methods: {
         discard() {
-            let vm = this;
-            $("#confirm-revert").modal({
-                onApprove() {
-                    vm.editor.set(vm.original);
-                    vm.changed = false;
-                }
-            }).modal('show');
+            if (this.confirmDiscard) {
+                this.changed = false;
+                this.editor.set(this.original);
+                this.confirmDiscard = false;
+            } else {
+                this.confirmDiscard = true;
+            }
 
         },
         save() {
             this.loading = true;
             this.success = false;
-            this.error = false;
             let json = JSON.stringify(this.editor.get());
             console.log("Saving json " + json);
             axios.patch('/dashboard/' + Server.id + '/censor', {
@@ -126,7 +132,7 @@ Vue.component('settings-censor', {
                     vm.success = false;
                 }, 10000);
             }).catch(resp => {
-                this.error = true;
+                toastr["error"]("An unknown error occurred. Please try again");
                 this.loading = false;
             })
         }
