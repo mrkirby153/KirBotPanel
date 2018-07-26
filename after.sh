6f67d7f2951ef7ee8abe2bd353cebe15ed2cfb92
mrkirby153 <mr.austinwhyte@gmail.com>
2018-07-25 21:30:58 -0700

    Expose redis to all interfaces when creating our vagrant box

--- after.sh
+++ after.sh
@@ -3,3 +3,7 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+# Open up redis to all interfaces
+sudo sed -i -e "s/bind/#bind/g" -e "s/protected-mode yes/protected-mode no/g" /etc/redis/redis.conf
+sudo systemctl restart redis.service
