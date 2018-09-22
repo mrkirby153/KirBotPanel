4d48dc7ed75e346ed7bda0a791974b3182ca13c3
mrkirby153 <mr.austinwhyte@gmail.com>
2018-09-21 20:29:53 -0700

    Make Server Settings soft deletable

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Server extends Model
 {
-    use DeletesRelations;
+    use DeletesRelations, SoftDeletes;
 
     public $table = "server_settings";
 
