15d8e160798173691563b8a6e935cf043b1a104c
mrkirby153 <mr.austinwhyte@gmail.com>
2019-02-17 15:12:26 -0800

    Fix server policy

--- app/Policies/ServerPolicy.php
+++ app/Policies/ServerPolicy.php
@@ -2,7 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\Server;
+use App\Models\Guild;
 use App\User;
 use App\Utils\PermissionHandler;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -29,11 +29,11 @@ class ServerPolicy
      * Determine whether the user can view the serverSettings.
      *
      * @param  \App\User $user
-     * @param  \App\Models\Server $serverSettings
+     * @param Guild $serverSettings
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function view(User $user, Server $serverSettings)
+    public function view(User $user, Guild $serverSettings)
     {
         return PermissionHandler::canView($user, $serverSettings->id);
     }
@@ -53,11 +53,11 @@ class ServerPolicy
      * Determine whether the user can update the serverSettings.
      *
      * @param  \App\User $user
-     * @param  \App\Models\Server $serverSettings
+     * @param Guild $serverSettings
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function update(User $user, Server $serverSettings)
+    public function update(User $user, Guild $serverSettings)
     {
         return PermissionHandler::canEdit($user, $serverSettings->id);
     }
@@ -66,21 +66,21 @@ class ServerPolicy
      * Determine whether the user can delete the serverSettings.
      *
      * @param  \App\User $user
-     * @param  \App\Models\Server $serverSettings
+     * @param Guild $serverSettings
      * @return mixed
      */
-    public function delete(User $user, Server $serverSettings)
+    public function delete(User $user, Guild $serverSettings)
     {
         return false;
     }
 
     /**
      * @param User $user
-     * @param Server $server
+     * @param Guild $server
      * @return bool
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function admin(User $user, Server $server)
+    public function admin(User $user, Guild $server)
     {
         return PermissionHandler::isAdmin($user, $server->id);
     }
