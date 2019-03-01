cceb45dd785ed193b5003e69c527b2082e8b80e9
mrkirby153 <mr.austinwhyte@gmail.com>
2019-02-28 23:00:02 -0800

    Add bootstrap modal component

---
+++ resources/assets/js/components/Modal.tsx
@@ -0,0 +1,88 @@
+import {Component} from 'react';
+import * as React from "react";
+import {isObject} from "util";
+
+
+interface ModalProps {
+    title: string,
+    onClose?: Function,
+    open: boolean
+}
+
+interface ModalState {
+    shown: boolean
+}
+
+export default class Modal extends Component<ModalProps, ModalState> {
+
+    private modalRef: React.RefObject<any>;
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            shown: false
+        };
+        this.modalRef = React.createRef();
+    }
+
+    componentWillReceiveProps(nextProps: Readonly<ModalProps>, nextContext: any): void {
+        if (nextProps.open == true) {
+            this.showModal();
+        } else {
+            this.hideModal();
+        }
+    }
+
+    showModal() {
+        if (!this.state.shown) {
+            $(this.modalRef.current).modal('show');
+            this.setState({
+                shown: true
+            })
+        }
+    }
+
+    hideModal() {
+        if (this.state.shown) {
+            $(this.modalRef.current).modal('hide');
+            this.setState({
+                shown: false
+            })
+        }
+    }
+
+    componentDidMount(): void {
+        $(this.modalRef.current).on('hidden.bs.modal', e => {
+            let fun = this.props.onClose;
+            if (fun) {
+                fun(e);
+            }
+            this.setState({
+                shown: false
+            })
+        });
+        if (this.props.open) {
+            this.showModal();
+        }
+    }
+
+    render() {
+        return (
+            <div className="modal fade" tabIndex={-1} role="dialog" ref={this.modalRef}>
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{this.props.title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="close">
+                                <span aria-hidden={true}>&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ resources/assets/js/types/ApiTypes.ts
@@ -0,0 +1,9 @@
+export interface Channel {
+    id: string,
+    server: string,
+    channel_name: string,
+    type: 'TEXT' | 'VOICE',
+    hidden: number,
+    created_at: string,
+    updated_at: string
+}
