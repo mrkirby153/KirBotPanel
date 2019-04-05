0383021382bff81aa5ccb3112827c612106fdb64
mrkirby153 <mr.austinwhyte@gmail.com>
2019-04-05 12:50:09 -0700

    Add form ref to form component

--- resources/assets/js/components/Form.tsx
+++ resources/assets/js/components/Form.tsx
@@ -6,23 +6,35 @@ interface FormProps {
 }
 
 export default class Form extends Component<FormProps, {}> {
+
+    private formRef: React.RefObject<HTMLFormElement>;
+
     constructor(props) {
         super(props);
 
+        this.formRef = React.createRef();
         this.handleOnSubmit = this.handleOnSubmit.bind(this);
     }
 
     handleOnSubmit(e) {
         e.preventDefault();
-        if(this.props.onSubmit) {
+        if (this.props.onSubmit) {
             this.props.onSubmit(e);
         }
+    }
+
+    getForm(): HTMLFormElement {
+        let val = this.formRef.current;
+        if (val == null) {
+            throw new Error("Form ref is null");
+        }
+        return val;
     }
 
     render() {
         return (
             <div>
-                <form className={this.props.busy ? 'busy' : ''} onSubmit={this.handleOnSubmit}>
+                <form className={this.props.busy ? 'busy' : ''} onSubmit={this.handleOnSubmit} ref={this.formRef}>
                     {this.props.children}
                 </form>
             </div>
