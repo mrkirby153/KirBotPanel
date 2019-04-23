5e427f6778fbd69fba212529b5bc26c0503daf49
mrkirby153 <mr.austinwhyte@gmail.com>
2019-04-22 20:54:58 -0700

    Remove old vue components

--- resources/assets/js/components_vue/ServerDashboard.js
+++
@@ -1,9 +0,0 @@
-require("./dashboard/General");
-require("./dashboard/Commands");
-require("./dashboard/Music");
-require("./dashboard/Channels");
-require("./dashboard/Permissions");
-require("./dashboard/Spam");
-require("./dashboard/Infractions");
-require("./dashboard/AntiRaid");
-require("./dashboard/AdminSettings");
--- resources/assets/js/components_vue/nav/bootstrapper.js
+++
@@ -1,6 +0,0 @@
-// Include neccecary components
-let navbarItem = require('./NavbarLink');
-let navbarDropdown = require('./NavbarDropdown');
-
-Vue.component('navbar-link', navbarItem);
-Vue.component('navbar-dropdown', navbarDropdown);
