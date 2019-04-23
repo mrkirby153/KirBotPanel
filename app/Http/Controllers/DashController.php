41dc636cb6136092cf293e4a8805320e1be24c7f
mrkirby153 <mr.austinwhyte@gmail.com>
2019-04-22 17:19:30 -0700

    Gut a lot of old code

---
+++ app/Http/Controllers/DashController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Guild;
+use Illuminate\Http\Request;
+use Intervention\Image\AbstractFont;
+
+class DashController extends Controller
+{
+    public function displayOverview(Request $request)
+    {
+        if (\Auth::guest()) {
+            return redirect(route('login') . '?returnUrl=' . urlencode($request->getRequestUri()));
+        }
+        $sub = \DB::table('server_permissions')->where('user_id', '=', \Auth::id())->select('server_id as id',
+            'name', 'icon_id')->join('guild', 'server_id', '=', 'guild.id');
+        $servers = \DB::table('guild')->where('owner', '=', \Auth::id())->select([
+            'id',
+            'name',
+            'icon_id'
+        ])->union($sub)->get();
+        return view('server.serverlist')->with(['servers' => $servers]);
+    }
+
+    public function redirectToAddUrl()
+    {
+        return redirect('https://discordapp.com/oauth2/authorize?client_id=' . env('DISCORD_KEY') . '&permissions=' . env('DISCORD_PERMISSIONS',
+                8) . '&scope=bot');
+    }
+
+    public function showDashboard(Guild $server)
+    {
+        $this->authorize('view', $server);
+        $server->load('channels');
+        $server->load('roles');
+        $server['readonly'] = !\Auth::user()->can('update', $server);
+        $server['owner'] = $server->owner == \Auth::id();
+        $server['admin'] = \Auth::user()->can('admin', $server);
+        \JavaScript::put([
+            'Server' => $server
+        ]);
+        return view('layouts.dashboard')->with([
+            'server' => $server
+        ]);
+    }
+
+    public function makeIcon(Request $request)
+    {
+        $serverName = $request->get('server_name');
+        $words = explode(" ", $serverName);
+        $acronym = "";
+        foreach ($words as $word) {
+            $acronym .= $word[0];
+        }
+
+        $img = \Image::canvas(290, 290, '#7289DA');
+        $img->text($acronym, 290 / 2, 290 / 2, function (AbstractFont $font) {
+            $font->file(public_path('Whitney-Book.ttf'));
+            $font->color("#FFFFFF");
+            $font->size(130);
+            $font->align('center');
+            $font->valign('middle');
+        });
+        return $img->response();
+    }
+
+    public function showArchived($key)
+    {
+        $data = Redis::get("archive:$key");
+        if ($data == null) {
+            return response('Archive not found or expired', 404);
+        }
+        if (starts_with($data, "e:")) {
+            $data = decrypt(substr($data, 2));
+        }
+        $response = Response::make($data, 200);
+        $response->header('Content-Type', 'text/plain');
+        $response->header('TTL', Redis::ttl("archive:$key"));
+        return $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,102 +15,24 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/serverIcon', 'Dashboard\GeneralController@makeIcon')->name('serverIcon');
+Route::get('/serverIcon', 'DashController@makeIcon')->name('serverIcon');
 
-Route::get('/servers', 'Dashboard\GeneralController@displayOverview')->name('dashboard.all');
+Route::get('/servers', 'DashController@displayOverview')->name('dashboard.all');
 Route::group(['middleware' => ['has_discord_token', 'can:view,server']], function () {
     Route::group(['prefix' => 'dashboard'], function () {
 
-        Route::get('/{server}/{any}', 'Dashboard\GeneralController@showDashboard')->where('any', '.*');
-        // General
-        Route::group(['middleware' => 'auth'], function () {
-            Route::get('/{server}', 'Dashboard\GeneralController@showDashboard')->name('dashboard.general');
-            Route::patch('/{server}/logging', 'Dashboard\GeneralController@updateLogging');
-            Route::post('/{server}/whitelist', 'Dashboard\GeneralController@updateChannelWhitelist');
-            Route::post('/{server}/botName', 'Dashboard\GeneralController@setUsername');
-            Route::patch('/{server}/persistence', 'Dashboard\GeneralController@setPersistence');
-            Route::patch('/{server}/muted', 'Dashboard\GeneralController@updateMutedRole');
-
-            Route::get('/{server}/api/infractions', 'Dashboard\GeneralController@retrieveInfractions');
-        });
-
-        // Commands
-        Route::group(['middleware' => 'auth'], function () {
-            Route::get('/{server}/commands', 'Dashboard\CommandController@showCommands')->name('dashboard.commands');
-            Route::patch('/{server}/command/{command}', 'Dashboard\CommandController@updateCommand');
-            Route::delete('/{server}/command/{command}', 'Dashboard\CommandController@deleteCommand');
-            Route::patch('/{server}/discriminator', 'Dashboard\CommandController@updateDiscrim');
-            Route::patch('/{server}/commandFail', 'Dashboard\CommandController@updateSilent');
-            Route::put('/{server}/commands', 'Dashboard\CommandController@createCommand');
-
-            Route::put('/{server}/commands/alias', 'Dashboard\CommandController@createAlias');
-            Route::delete('/{server}/commands/alias/{alias}', 'Dashboard\CommandController@deleteAlias');
-        });
-
-        // Music
-        Route::group(['middleware' => 'auth'], function () {
-            Route::get('/{server}/music', 'Dashboard\MusicController@index')->name('dashboard.music');
-            Route::post('/{server}/music', 'Dashboard\MusicController@update');
-        });
-
-        Route::group(['middleware' => 'auth'], function () {
-            Route::get('/{server}/permissions',
-                'Dashboard\PermissionController@showPane')->name('dashboard.permissions');
-            Route::post('/{server}/permissions/{permission}', 'Dashboard\PermissionController@update');
-            Route::put('/{server}/permissions', 'Dashboard\PermissionController@create');
-            Route::delete('/{server}/permissions/{permission}', 'Dashboard\PermissionController@delete');
-
-            Route::get('/{server}/infractions',
-                'Dashboard\GeneralController@showInfractions')->name('dashboard.infractions');
-            Route::get('/{server}/infractions/{infraction}', 'Dashboard\GeneralController@showInfraction');
-
-            Route::get('/{server}/rolePermissions', 'Dashboard\PermissionController@getRolePermissions');
-            Route::put('/{server}/rolePermissions', 'Dashboard\PermissionController@createRolePermission');
-            Route::delete('/{server}/rolePermissions/{permission}',
-                'Dashboard\PermissionController@deleteRolePermission');
-            Route::patch('/{server}/rolePermissions/{permission}',
-                'Dashboard\PermissionController@updateRolePermission');
-        });
-
-        Route::get('/{server}/spam', 'Dashboard\SpamController@index')->name('dashboard.spam');
-        Route::patch('/{server}/spam', 'Dashboard\SpamController@updateSettings')->name('dashboard.spamUpdate');
-        Route::patch('/{server}/censor', 'Dashboard\SpamController@updateCensor')->name('dashboard.censorUpdate');
-
-        // Logging
-        Route::group([], function () {
-            Route::put('/{server}/logSetting', 'Dashboard\GeneralController@createLogSetting');
-            Route::delete('/{server}/logSetting/{setting}', 'Dashboard\GeneralController@deleteLogSetting');
-            Route::patch('/{server}/logSetting/{setting}', 'Dashboard\GeneralController@updateLogSetting');
-        });
-
-        // Anti-Raid
-        Route::group(['middleware' => 'auth'], function () {
-            Route::get('/{server}/anti-raid', 'Dashboard\AntiRaid@index')->name('dashboard.raid');
-            Route::patch('/{server}/anti-raid', 'Dashboard\AntiRaid@update')->name('dashboard.raid.update');
-        });
+        Route::get('/{server}/{any}', 'DashController@showDashboard')->where('any', '.*');
     });
 });
-// Starboard
-Route::patch('/dashboard/{guild}/starboard',
-    'Dashboard\GeneralController@updateStarboard')->name('dashboard.starboard')->middleware('auth');
 
-Route::get('/archive/{key}', 'Dashboard\GeneralController@showArchived');
+Route::get('/archive/{key}', 'DashController@showArchived');
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/settings', 'AdminController@settings')->name('admin.settings');
-    Route::post('/settings/{guild}/{key}', 'AdminController@updateSettings');
-    Route::delete('/settings/{guild}/{key}', 'AdminController@clearSettings');
     Route::get('/', 'AdminController@show')->name('admin.main');
-});
-
-Route::group(['prefix' => 'server'], function () {
-    Route::get('/{server}/commands', 'Dashboard\GeneralController@showCommandList');
-    Route::get('/{server}/queue', 'Dashboard\MusicController@displayQueue');
-    Route::post('/{server}/queue', 'Dashboard\MusicController@webQueue');
-    Route::get('/{server}/quotes', 'Dashboard\GeneralController@showQuotes');
 });
 
 Route::get('login', 'AuthController@doLogin')->name('login.do');
 Route::get('/auth/login', 'AuthController@showLogin')->name('login');
 Route::get('logout', 'AuthController@logout')->name('logout');
-Route::get('/add', 'Dashboard\GeneralController@redirectToAddUrl')->name('add');
+Route::get('/add', 'DashController@redirectToAddUrl')->name('add');
