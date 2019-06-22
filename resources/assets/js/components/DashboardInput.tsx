2b2952006116d6055f8eaceb6f7c1df22eabd885
mrkirby153 <mr.austinwhyte@gmail.com>
2019-06-22 14:21:23 -0700

    Forward refs on dash inputs

--- resources/assets/js/components/DashboardInput.tsx
+++ resources/assets/js/components/DashboardInput.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import Switch, {SwitchProps} from "./Switch";
 
-export const DashboardInput = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
+type Ref = HTMLInputElement
+
+export const DashboardInput = forwardRef<Ref, React.InputHTMLAttributes<Ref>>((props, ref) => {
     let {disabled, ...rest} = props;
     if (window.Panel.Server.readonly) {
         disabled = true;
     }
-    return <input disabled={disabled} {...rest}/>
-};
+    return <input disabled={disabled} ref={ref} {...rest}/>
+});
 
 export const DashboardSelect = (props: React.SelectHTMLAttributes<HTMLSelectElement>) => {
     let {disabled, ...rest} = props;
