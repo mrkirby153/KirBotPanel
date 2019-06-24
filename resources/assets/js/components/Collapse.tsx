5c258594b1aaef3727f80f313acd1960b19e4ca4
mrkirby153 <mr.austinwhyte@gmail.com>
2019-06-23 23:31:21 -0700

    Add collapse component

---
+++ resources/assets/js/components/Collapse.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+interface CollapseProps {
+    visible: boolean
+}
+
+export default class Collapse extends React.Component<CollapseProps, {}> {
+
+    private ref: React.RefObject<HTMLDivElement>;
+    private shouldRemove: boolean;
+    private registered: boolean;
+
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+        this.shouldRemove = false;
+        this.registered = false;
+
+        this.expand = this.expand.bind(this);
+        this.collapse = this.collapse.bind(this);
+        this.removeStyle = this.removeStyle.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps: Readonly<CollapseProps>, nextContext: any): void {
+        if (nextProps.visible) {
+            this.shouldRemove = true;
+            this.expand()
+        } else {
+            this.shouldRemove = false;
+            this.collapse()
+        }
+    }
+
+
+    removeStyle() {
+        let el = this.ref.current;
+        if (el && this.shouldRemove) {
+            this.shouldRemove = false;
+            el.style.height = null;
+            el.classList.add('visible');
+            el.removeEventListener('transitioned', this.removeStyle);
+        }
+    }
+
+    expand() {
+        let element = this.ref.current;
+        if (!element) {
+            console.warn("Collapse ref was null?");
+            return;
+        }
+
+        let height = element.scrollHeight;
+        element.style.height = height + 'px';
+        if (!this.registered) {
+            this.registered = true;
+            element.addEventListener('transitionend', this.removeStyle);
+        }
+    }
+
+    collapse() {
+        let element = this.ref.current;
+        if (!element) {
+            console.warn("Collapse ref was null?");
+            return;
+        }
+
+        let height = element.scrollHeight;
+        let prevTransition = element.style.transition;
+        element.style.transition = '';
+
+        requestAnimationFrame(() => {
+            if (element != null) {
+                element.style.height = height + 'px';
+                element.style.transition = prevTransition;
+
+                requestAnimationFrame(() => {
+                    if (element != null) {
+                        element.style.height = 0 + 'px';
+                        element.classList.remove('visible');
+                    }
+                })
+            }
+        })
+    }
+
+    componentDidMount(): void {
+        if (!this.props.visible) {
+            // Hide the component
+            let el = this.ref.current;
+            if (el) {
+                el.style.height = 0 + 'px';
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="collapsible" ref={this.ref}>
+                {this.props.children}
+            </div>
+        );
+    }
+
+}
